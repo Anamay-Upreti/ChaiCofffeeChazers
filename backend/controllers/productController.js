1075f3fa4541d9b3d8ba1c876085d4e53e8500f7
Anamay-Upreti <rcupretidgc13@gmail.com>
2025-05-07 23:24:53 +0530

    added controller for products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,19 @@
+import Product from "../models/Product";
+
+export const createProduct = async (req, res) => {
+    const { name, description, price, image } = req.body;
+    try {
+        const product = new Product({
+            name,
+            description,
+            price,
+            image,
+            createdBy: req.user._id
+        })
+
+        await product.save();
+        res.status(201).json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
