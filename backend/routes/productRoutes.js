16a48cf7fb29520ce5059953eea98238f3f8eca7
Anamay-Upreti <rcupretidgc13@gmail.com>
2025-05-08 23:49:17 +0530

    productcomplete

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
+import upload from "../middlewares/uploadMiddleware.js";
 import {
-    createProduct } from "../controllers/productController.js";
-import  protect from "../middlewares/authMiddleware.js";
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct  
+} from "../controllers/productController.js";
+import protect from "../middlewares/authMiddleware.js";
+import adminOnly from "../middlewares/isAdmin.js";
 
 const router = express.Router();
 
-router.post("/", protect, createProduct);
+router.post("/", protect, adminOnly, upload.single("image"), createProduct);
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.put("/:id", protect, adminOnly, upload.single("image"), updateProduct);
+router.delete("/:id", protect, adminOnly, deleteProduct);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use('/uploads', express.static(path.resolve('uploads')));
